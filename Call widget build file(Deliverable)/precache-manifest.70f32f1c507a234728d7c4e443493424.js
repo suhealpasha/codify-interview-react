62799fc20c8692ebe3b47b79b35fbdf667c4afb3
suheal <suhalpasha@gmail.com>
2020-07-08 20:08:01 +0530

    Codify upload

---
+++ Call widget build file(Deliverable)/precache-manifest.70f32f1c507a234728d7c4e443493424.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0b17f31f64017a6e40dc476f0fbc480b",
+    "url": "/index.html"
+  },
+  {
+    "revision": "9910b3ecb451704cdc13",
+    "url": "/static/css/main.min.css"
+  },
+  {
+    "revision": "9910b3ecb451704cdc13",
+    "url": "/static/js/main.37804066.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/main.37804066.js.LICENSE.txt"
+  }
+]);
